fdff561ccc275f824e0ed824d1ba1ab6eb26c878
Jenkins <jenkins@BradleyohnsMini.attlocal.net>
2015-02-13 01:30:55 -0600

    Update mingw-w64 to use latest tag (including rc), add widl, secure

--- src/mingw-w64.mk
+++ src/mingw-w64.mk
@@ -3,16 +3,18 @@
 
 PKG             := mingw-w64
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.3.0
-$(PKG)_CHECKSUM := d31eac960d42e791970697eae5724e529c81dcd6
+$(PKG)_VERSION  := 4.0-rc2
+$(PKG)_CHECKSUM := 6f046e495d5eac0560e7585c58cb801d932e85fa
 $(PKG)_SUBDIR   := $(PKG)-v$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-v$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(PKG)-release/$($(PKG)_FILE)
-$(PKG)_DEPS     :=
+$(PKG)_DEPS     := widl
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/' | \
-    $(SED) -n 's,.*mingw-w64-v\([0-9.]*\)\.tar.*,\1,p' | \
+    git ls-remote --tags git://git.code.sf.net/p/mingw-w64/mingw-w64 | \
+    cut -d '/' -f 3 | \
+    cut -d '^' -f 1 | \
+    cut -c 2- | \
     $(SORT) -V | \
     tail -1
 endef
@@ -22,7 +24,9 @@ define $(PKG)_BUILD_mingw-w64
     cd '$(1).headers-build' && '$(1)/mingw-w64-headers/configure' \
         --host='$(TARGET)' \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --enable-sdk=all
+        --enable-sdk=all \
+        --enable-secure \
+        --enable-idl
     $(MAKE) -C '$(1).headers-build' install
 endef
 
